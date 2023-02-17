fd58f2d44ca024a4e37ba7bfa1afe81a06853753
komalsawane2018 <komalsawane2018@gmail.com>
2023-02-17 16:05:50 +0530

    02_typesOf Assignment Completed
    02_typesOf Assignment Completed

---
+++ 02_typeOf.js
@@ -0,0 +1,34 @@
+console.log("************************1st. Step************************");
+var bankName;
+console.log("**** Before Initialization ****");
+console.log("Bank Name:",bankName,"Data type:" ,typeof bankName );
+bankName = 'SBI Bank';
+console.log("**** After Initialization ****");
+console.log("Bank Name:", bankName, "Data type:", typeof bankName);
+
+console.log("**************************2nd Step************************");
+var marks = '90%';
+console.log("Marks:", marks, "Data type:", typeof marks);
+
+console.log("**************************3rd Step************************");
+ var isWorking;
+ console.log("**** Before Initialization ****");
+ console.log("Working Status:", isWorking, "Data type:",typeof isWorking);
+
+isWorking = true;
+console.log("**** After Initialization ****");
+console.log(" Is Working:", isWorking, "Data type:",typeof isWorking);
+
+ console.log("**************************4th Step************************");
+
+ var totalCount = "Hundred and Seven";
+ console.log("Total Count:", totalCount, "Data type:", typeof totalCount);
+
+ console.log("*****  Update Variable with 107  ******");   
+ 
+totalCount = 107;
+console.log("Total Count:", totalCount, "Data Type:", typeof totalCount);
+ 
+
+
+
