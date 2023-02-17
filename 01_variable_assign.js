3273f1f2f4981c154cd913bee5a030f7164b8232
komalsawane2018 <komalsawane2018@gmail.com>
2023-02-17 06:52:03 +0530

    variable_assign completed
    variable_assign completed

---
+++ 01_variable_assign.js
@@ -0,0 +1,22 @@
+console.log("******************* 1st.Step******************");
+var myFullName;
+console.log(undefined);
+var myFullName = "Komal Sawane";
+console.log("My Full Name:", myFullName)
+var myFullName ="Komal Latesh Sawane";
+console.log("My Full Name:",myFullName);
+
+
+console.log("******************  2nd. Step******************");
+var myLove = "I Love Only JS";
+console.log(myLove);
+
+
+console.log("******************* 3rd. Step*******************");
+var firstName ="Komal";
+var lastName ="Sawane";
+var age = "29";
+var address = "Hadapsar, Pune-412308";
+var gender = "Female";
+var isYouMarried = "yes";
+console.log("First Name:", firstName, "Last Name:", lastName,"Age:", age, "Address:",address," Gender:",gender, "Is You Married:",isYouMarried);
