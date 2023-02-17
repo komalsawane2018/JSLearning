b591785113afcb35597d180d5663f02bf24bf556
komalsawane2018 <komalsawane2018@gmail.com>
2023-02-17 15:34:16 +0530

    01_Variable Basic Assignment Completed
    01_Variable Basic Assignment Completed

---
+++ 01_variableBasicAssign.js
@@ -0,0 +1,32 @@
+console.log("**************1st Step Data Type is Number***************");
+var age = 29;
+console.log("Age:", age, "Data type:", typeof age);
+var weight = 52;
+console.log("Weight:", weight, "Data type:", typeof weight);
+var mobileNo = 12563488;
+console.log( "Mobile No:", mobileNo, "Data type:", typeof mobileNo); 
+
+console.log("**************String*****************");
+var myName = "KOMAL";
+console.log( "My Name:", myName,"Data type:", typeof myName);
+var date = "16/02/2023";
+console.log("Data:", date,"Data type:", typeof date);
+var city =  "Pune";
+console.log( "City Name:", city,"Data type:", typeof city);
+
+console.log("***************Boolean*************************");
+var isYouMarried = true;
+console.log("Marital Status:", isYouMarried,"Data type:",typeof isYouMarried );
+var isYourGraduate = true;
+console.log("Graduation:", isYourGraduate, "Data type:", typeof isYourGraduate);
+
+
+console.log("*********************2nd Step*************************");
+
+var name = null;
+console.log("My Name:", name, typeof name);
+console.log("*****************Undefined*************************");
+var ABC;
+console.log("ABC:", ABC,"Data type:", typeof ABC);
+
+
