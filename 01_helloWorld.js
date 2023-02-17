627bb8f58f62e582f3e02312dd90840650eccba6
komalsawane2018 <komalsawane2018@gmail.com>
2023-02-17 16:22:14 +0530

    Updated code data type
    Updated code data type

---
+++ 01_helloWorld.js
@@ -0,0 +1,15 @@
+console.log(" Komal Sawane");
+var fullName = "Komal Sawane";
+console.log(fullName);
+console.log(fullName);
+
+
+var city; // Variable Declration
+console.log(city);
+city="Pune"; // Variable Initialization
+console.log(city);
+city="Surat"; // Variable Update
+console.log(city);
+
+var universityName ="Pune University";
+console.log(universityName);
