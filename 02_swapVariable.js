78d282d3fa3e140162da0d1bc8900a067b4b646e
komalsawane2018 <komalsawane2018@gmail.com>
2023-02-20 17:13:55 +0530

    Updated file
    Updated file

--- 02_swapVariable.js
+++ 02_swapVariable.js
@@ -18,10 +18,14 @@ var num2 = 200;
 var num3 = 300;
 console.log("************ Before swapping***************");
 console.log(num1,num2,num3);
-var temp = num2;
- num2 = num1;
- num1 = num3;
- num3 = temp;
+var temp = num1;
+num1=num2; //200
+num2=num3; //300
+num3=temp; //100
+// var temp = num2;
+//  num2 = num1;
+//  num1 = num3;
+//  num3 = temp;
  console.log("**************After Swapping**************");
  console.log(num1, num2,num3);
 
