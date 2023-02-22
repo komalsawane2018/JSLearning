3fa1ecbcfef96a066be880c70a0aab1889e857b6
komalsawane2018 <komalsawane2018@gmail.com>
2023-02-22 12:41:52 +0530

    Updated File String Template
    Updated File String Template

---
+++ 04_string Template.js
@@ -0,0 +1,11 @@
+
+
+
+console.log("You are champ man........");
+console.log(`You are "champ" man......`);
+
+var  firstName = "Komal";
+var lastName   = "Sawane";
+var city       =  "Pune";
+console.log("My first Name:", firstName, "last name:", lastName, "City:", city);  // old method to log string
+console.log(`My First Name ${firstName} Last Name ${lastName} City ${city} `);  // string Template with variable substitution
