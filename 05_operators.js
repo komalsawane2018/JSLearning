ca58cb4aa402ffb70e4cea9dc3d45169d2970ecf
komalsawane2018 <komalsawane2018@gmail.com>
2023-02-22 12:42:24 +0530

    Upadated file Operators
    Upadated file Operators

---
+++ 05_operators.js
@@ -0,0 +1,46 @@
+console.log(`========Arithmatic Operators========`);
+
+var num1 = 10;
+var num2 = 2;
+var result = num1+num2;     // Additon
+console.log(`Addition is ${result}`);
+
+result = num1 - num2;
+console.log(`Substraction is ${result}`);   // Subtraction
+
+result = num1 * num2;
+console.log(`Multiplication is ${result}` );   // Multiplication ;
+
+result = num1 ** num2;
+console.log(`Exponetional is ${result}`);   // 10*10;  square of num
+
+result = num1 / 3
+console.log(`Division is ${result}`);
+
+result = num1 % 3;
+console.log(`Modulus is ${result}`);   // give reminder
+
+var num = 10;
+num+=20;   //num= num + 20;
+console.log(`Compound Addition is ${num}`);
+
+console.log(`====Comparison Operator========`);
+var num3 = "10";  // string
+var num4 = 10;    // number
+console.log(num3==10);  //  coversion 10==10
+
+console.log(num3==num4);     // conversion and compare value
+
+console.log(num3=== num4);   // no coversion only compare value 
+
+console.log(num3 != num4);  // not equal to num4
+
+console.log(10 > 20);  //greater than
+
+console.log(20 < 10);  //less than
+
+console.log(10 < 10);  //less than
+
+var marks = result;
+console.log(marks >= 35);   // Greater than equal
+
