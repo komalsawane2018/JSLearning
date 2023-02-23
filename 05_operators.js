1728e5ccb39796bbe366d221270e9bbd67b1bf68
komalsawane2018 <komalsawane2018@gmail.com>
2023-02-23 13:45:20 +0530

    Updated Operator File
    Updated Operator File

--- 05_operators.js
+++ 05_operators.js
@@ -44,3 +44,39 @@ console.log(10 < 10);  //less than
 var marks = result;
 console.log(marks >= 35);   // Greater than equal
 
+console.log(`=================Ternary Operators=============`)
+var marks = 70;
+var result = marks >= 60 ? "Allow Him for the Interview" : "Don't allow";
+console.log(result);
+
+var age = 20;
+var res = age>=21 ? "True" : "False";
+console.log(res);
+
+console.log(`To check number EVEN or ODD`);
+myName = 7;
+
+var result = myName % 2 == 0 ? "EVEN" : "ODD";   // % Modulas
+console.log(result);
+
+function wordLength(word){
+    var wordLength = word.length;
+    var result = wordLength%2==0 ? "EVEN" : "ODD";
+    return result;
+ }
+ var result = wordLength("JavaScript");
+ console.log(`Word "JavaScript" has ${result} length` );
+ // var result = wordLength("Google");
+ console.log(`Word "Google" has ${wordLength("Google Chrome")} length` );
+
+
+ function maleMarriageEligibility(gender, age, boyName){
+    var result = (gender=="Male" && age>=21) ? `Hey ${boyName} you are eligible for marriage` : " No Try next time" ;
+    return result;
+ }
+  var result = maleMarriageEligibility("Male", 25, "Billgates");
+  console.log(result);
+
+
+
+
