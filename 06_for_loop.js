c033a6c74557488ec3f3fafeb66b4d6319aac052
komalsawane2018 <komalsawane2018@gmail.com>
2023-03-01 11:10:43 +0530

    Updating 06_ for_ loop file
    06_Updating for loop file.

---
+++ 06_for_loop.js
@@ -0,0 +1,32 @@
+
+// Print Number From 1 to 10  for this 3 step
+//1.initialization 1
+//2. Update Expression +1 (increment)
+//3.Condition <=10
+
+for (let index = 1; index <=10 ;index= index+1) { 
+    
+    console.log(index);
+}
+
+// "Reverse Numbers"
+
+for (let index = 10; index >=0; index= index - 1) {
+console.log(index);
+ }
+
+ // Table of 5
+
+ for (let index = 5; index <= 50; index= index + 5){
+  console.log(index);
+}
+
+for (let index = 1; index <10;) {
+    console.log(index);
+    
+}
+
+for (let index = 10; index >= 0; index++) {
+console.log(index);
+    
+}
