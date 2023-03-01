22c05d8157056a2ba615b87c7bb596dcbd483604
komalsawane2018 <komalsawane2018@gmail.com>
2023-03-01 11:12:09 +0530

    Completed 06_for_loopAssignment 01
    Completed 06_for_loopAssignment 01

---
+++ 06_for_loopAssign01.js
@@ -0,0 +1,46 @@
+console.log(`---------------------Step 1.-------------------------------------`);
+console.log(`Print the number from 5 to 15 by incrementing 1`);
+for (let index = 5; index <= 15; index++) {
+    console.log(`${index}`);
+    
+}
+console.log(`---------------------Step 2. -------------------------------------`);
+
+console.log(`Prints the number from 50 to 40 by decrementing by 1 `); 
+for (let index = 50; index >= 40; index = index-1) {
+    console.log(index);
+    
+}
+console.log(`----------------------Step 3. ------------------------------------`);
+
+console.log(`Find first 15 odd Numbers`);
+for (let index = 1; index <= 30; index = index+2) {
+    console.log(index);
+    
+}
+console.log(`----------------------Step 4. ------------------------------------`);
+console.log(`Find first 10 Even Numbers`);
+for (let index = 0; index < 20; index = index + 2 ) {
+console.log(index);
+    
+}
+console.log(`----------------------Step 5. ------------------------------------`);
+
+console.log(`Print table of 5`);
+for (let index = 5; index<=50; index = index+5) {
+    console.log(index);
+    
+}
+console.log(`----------------------Step 6. ------------------------------------`);
+console.log(`Print table of 10`);
+for (let index = 10; index <=100; index= index+10) {
+    console.log(index) ;
+    
+}
+console.log(`----------------------Step 7. ------------------------------------`);
+console.log(`Reverse Number 100 to 10`);
+
+for (let index = 100; index >= 0; index= index - 10) {
+console.log (index);
+    
+}
