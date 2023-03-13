12773b217a17642b6a84ce50bad126524b541ca5
komalsawane2018 <komalsawane2018@gmail.com>
2023-03-13 11:47:53 +0530

    Updated 10_ Constructor_Function
    Updated 10_ Constructor_Function

---
+++ 10_constructor_Function.js
@@ -0,0 +1,31 @@
+                   
+    function Person(fullName, city){     // function is constructor function thats why P is captil
+
+        this.fullName = fullName;
+        this.city = city;
+        this.show = function(){
+            console.log(`${this.fullName} ${this.city}`);
+        }
+    }
+    Person.prototype.country = "India";
+    
+    const dhoni = new Person("MS Dhoni", "Ranchi");
+    dhoni.show();
+    console.log(dhoni.country);
+    
+    const gill = new Person("Shubam Gill", "Mumbai");
+    gill.show();
+    console.log(gill.country);
+    
+
+    
+    const ash = new Person("R Ashwin", "Chenai");
+    ash.show();
+    console.log(ash.country);
+
+
+const date = new Date();
+console.log(typeof date);          // Built in 
+
+ const result = Math.pow(2,3);
+ console.log(result);
