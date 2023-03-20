7ccd8d838c43a381a8c814f25ad2b4ff1b1da207
komalsawane2018 <komalsawane2018@gmail.com>
2023-03-20 12:16:44 +0530

    Updating Files
    Updating Files

---
+++ 14_call_Backs.js
@@ -0,0 +1,39 @@
+
+function doAssignment(callback){
+    // Ajay 
+    console.log("Solving assignments");
+    console.log("Step 1 to 10 all ares solved now");
+    callback();
+}
+function copyAssignment(){
+    // Ajinkya
+    console.log("Thank you Ajju, let me copy assignments!");
+    console.log("Ohh finally coped all assignment");
+}
+doAssignment(copyAssignment);            // passing function as a argument
+
+console.log(`============ Shortest ex =================`);
+function greet(){
+    console.log("Good Morning.....");
+}
+// 1 sec = 1000 milli seconds
+// 3 sec = 3000 ms
+setTimeout(greet, 4000);
+
+function firstClass(){
+    console.log("We can return function from another function");
+    return function(){
+        console.log("Inner function");
+    }
+    // return innerFunction;
+}
+const inner = firstClass();
+inner();
+
+
+
+
+let result = function show(){
+    console.log(" I am learning JS");
+}
+result()
---
+++ 15_spread_Operator.js
@@ -0,0 +1,8 @@
+
+
+// ... Rest Parameter
+function show(arg1, ...arg){
+    console.log(arg1, arg);
+
+}
+show(3, 4, 6, 8, 9);
